3e8e26232e6b8a7c9e5015d6ea32f35ed5069974
Jogchem Dijkstra <jogchem@gmail.com>
2018-10-21 22:46:19 +0200

    Step 5

--- cypress/integration/autos.spec.js
+++ cypress/integration/autos.spec.js
@@ -14,4 +14,16 @@ describe('Autos', () => {
       .its('body')
       .should('eq', 'Cypress toegevoegd met id 1');
   });
+
+  it('should require field merk', () => {
+    cy.request({
+      method: 'POST',
+      url: '/autos',
+      body: {bouwjaar: 2010},
+      failOnStatusCode: false
+    }).should(response => {
+      expect(response.status).to.eq(500);
+      expect(response.body).to.contain('Auto moet minstens een merk bevatten');
+    });
+  });
 });
