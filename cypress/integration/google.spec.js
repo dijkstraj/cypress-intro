01d8c61b90bf8de4643b4d6e93ac912d2bd47ce6
Jogchem Dijkstra <jogchem@gmail.com>
2018-10-22 20:22:57 +0200

    Step 9

---
+++ cypress/integration/google.spec.js
@@ -0,0 +1,17 @@
+describe('Autos', () => {
+  beforeEach(() => {
+    // Given I visit Google
+    cy.visit('https://www.google.com');
+  });
+
+  it('should find the answer to life the universe and everything', () => {
+    // When I search for "answer to life the universe and everything"
+    cy.get('.sbibtd input').first() // <-- search field
+      .type('answer to life the universe and everything')
+      .type('{enter}');
+
+    // Then I get 42
+    cy.get('#cwos') // <-- output field of calculator
+      .should('have.text', '42');
+  });
+});
