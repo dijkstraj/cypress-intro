f12764840fbd15ec8a98294962531ca5375b0d81
Jogchem Dijkstra <jogchem@gmail.com>
2018-10-28 21:12:19 +0100

    Step 11

--- cypress/integration/stubbing.spec.js
+++ cypress/integration/stubbing.spec.js
@@ -3,4 +3,13 @@ describe('Comments', () => {
     cy.server();
     cy.visit('https://example.cypress.io/commands/network-requests');
   });
+
+  it('should display the body of the comment', () => {
+    cy.route('GET', '/comments/*', {
+      body: 'Success is a science; if you have the conditions, you get the result.'
+    });
+    cy.get('.network-btn.btn-primary').click();
+    cy.get('.network-comment')
+      .should('have.text', 'Success is a science; if you have the conditions, you get the result.');
+  });
 });
